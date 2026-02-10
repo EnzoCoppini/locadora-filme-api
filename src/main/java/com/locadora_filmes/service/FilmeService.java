ea3c9c471a12d725f20be25baebf73d9a8de2e72
EnzoCoppini <enzo.coppini7@gmail.com>
2026-02-10 14:23:52 -0300

    Criação das classes filme e locacao service

--- src/main/java/com/locadora_filmes/service/FilmeService.java
+++ src/main/java/com/locadora_filmes/service/FilmeService.java
@@ -1,4 +1,16 @@
 package com.locadora_filmes.service;
 
+
+import com.locadora_filmes.repository.FilmeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FilmeService {
+
+
+    private final FilmeRepository filmeRepository;
+
+    public FilmeService(FilmeRepository filmeRepository){
+        this.filmeRepository = filmeRepository;
+    }
 }
--- src/main/java/com/locadora_filmes/service/LocacaoService.java
+++ src/main/java/com/locadora_filmes/service/LocacaoService.java
@@ -1,15 +1,19 @@
 package com.locadora_filmes.service;
 
 
+import com.locadora_filmes.repository.FilmeRepository;
 import com.locadora_filmes.repository.LocacaoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Locacao {
+public class LocacaoService {
 
     private final LocacaoRepository locacaoRepository;
+    private final FilmeRepository filmeRepository;
 
-    public void 
-
+    public LocacaoService(LocacaoRepository locacaoRepository, FilmeRepository filmeRepository){
+        this.locacaoRepository = locacaoRepository;
+        this.filmeRepository = filmeRepository;
+    }
 
 }
