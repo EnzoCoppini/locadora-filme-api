6ffd50755191dd7e465b61f6bf0d8430ccbdc16b
EnzoCoppini <enzo.coppini7@gmail.com>
2026-02-10 14:17:45 -0300

    Adicionando anotação @Repository

--- src/main/java/com/locadora_filmes/repository/LocacaoRepository.java
+++ src/main/java/com/locadora_filmes/repository/LocacaoRepository.java
@@ -2,6 +2,8 @@ package com.locadora_filmes.repository;
 
 import com.locadora_filmes.entity.Locacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocacaoRepository extends JpaRepository<Locacao, Long> {
 }
