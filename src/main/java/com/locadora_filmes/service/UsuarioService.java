ea862888c9ebd6d306926de40741854e4d665628
EnzoCoppini <enzo.coppini7@gmail.com>
2026-02-11 16:17:10 -0300

    Adicionando métodos para usar no controller

--- src/main/java/com/locadora_filmes/service/UsuarioService.java
+++ src/main/java/com/locadora_filmes/service/UsuarioService.java
@@ -2,6 +2,8 @@ package com.locadora_filmes.service;
 
 
 import com.locadora_filmes.entity.Usuario;
+import com.locadora_filmes.exception.EntidadeNaoEncontradaException;
+import com.locadora_filmes.exception.RegraDeNegocioException;
 import com.locadora_filmes.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,17 +20,49 @@ public class UsuarioService {
     }
 
     public Usuario cadastrarUsuario(Usuario usuario){
-        if(usuarioRepository.findByEmail(usuario.getEmailUsuario()).isPresent()){
-            throw new RuntimeException("Email ja cadastrado");
+        if(usuarioRepository.findByEmailUsuario(usuario.getEmailUsuario()).isPresent()){
+            throw new RegraDeNegocioException("O email " + usuario.getEmailUsuario() + " ja está em uso");
         }
         return usuarioRepository.save(usuario);
     }
 
     public List<Usuario> listarUsuarios(){
+        if(usuarioRepository.count() == 0){
+            throw new EntidadeNaoEncontradaException("Nenhum cadastro feito no sistema.");
+        }
         return usuarioRepository.findAll();
     }
 
-    public Optional<Usuario> listarPorEmail(String email){
-        return usuarioRepository.findByEmail(email);
+    public Usuario listarPorEmail(String emailUsuario){
+        if(usuarioRepository.count() == 0){
+            throw new EntidadeNaoEncontradaException("Nenhum cadastro feito no sistema.");
+        }
+        return usuarioRepository.findByEmailUsuario(emailUsuario).orElseThrow(() -> new EntidadeNaoEncontradaException("Email: " +emailUsuario + " não encontrado."));
     }
+
+    public Usuario listarPorId(Long id){
+        return usuarioRepository.findById(id).orElseThrow(() -> new EntidadeNaoEncontradaException("Id: " + id + " não encontrado."));
+    }
+
+    public void deletarPorId(Long id){
+       if (!usuarioRepository.existsById(id)){
+           throw new EntidadeNaoEncontradaException("Não foi possível deletar. ID: " + id + " não encontrado.");
+       }
+        usuarioRepository.deleteById(id);
+    }
+
+    public Usuario atualizarUsuario(Long id, Usuario usuarioAtualizado){
+        Usuario usuarioExistente = usuarioRepository.findById(id).orElseThrow(() -> new EntidadeNaoEncontradaException("Id: " + id + " não encontrado"));
+
+        usuarioExistente.setNomeUsuario(usuarioAtualizado.getNomeUsuario());
+        usuarioExistente.setEmailUsuario(usuarioAtualizado.getEmailUsuario());
+        usuarioExistente.setSenhaUsuario(usuarioAtualizado.getSenhaUsuario());
+        usuarioExistente.setCpfUsuario(usuarioAtualizado.getCpfUsuario());
+        usuarioExistente.setDataNascimento(usuarioAtualizado.getDataNascimento());
+        usuarioExistente.setTipo(usuarioAtualizado.getTipo());
+
+        return usuarioRepository.save(usuarioExistente);
+    }
+
+
 }
