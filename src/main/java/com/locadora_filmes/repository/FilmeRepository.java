fbd3567af1e47eef93148cc0c600a09cfa2dc8c0
EnzoCoppini <enzo.coppini7@gmail.com>
2026-02-13 16:42:07 -0300

    Adicionando método existsByTituloIgnoreCase

--- src/main/java/com/locadora_filmes/repository/FilmeRepository.java
+++ src/main/java/com/locadora_filmes/repository/FilmeRepository.java
@@ -4,8 +4,13 @@ import com.locadora_filmes.entity.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
 
 
+    Optional<Filme> findByTituloIgnoreCase(String titulo);
+
+    Boolean existsByTituloIgnoreCase(String titulo);
 }
