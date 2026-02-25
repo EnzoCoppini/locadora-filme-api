e619d9945322deca65cde76164c2aa1731fbb759
EnzoCoppini <enzo.coppini7@gmail.com>
2026-02-25 15:52:08 -0300

    adicionando método findByUsuarioIdAndDevolvidoFalse e findByUsuarioId para usar no service

--- src/main/java/com/locadora_filmes/repository/LocacaoRepository.java
+++ src/main/java/com/locadora_filmes/repository/LocacaoRepository.java
@@ -4,6 +4,13 @@ import com.locadora_filmes.entity.Locacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LocacaoRepository extends JpaRepository<Locacao, Long> {
+
+    List<Locacao> findByUsuarioIdAndDevolvidoFalse(Long usuarioId);
+    List<Locacao> findByUsuarioId(Long usuarioId);
+
+
 }
